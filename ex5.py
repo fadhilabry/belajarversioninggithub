36e2b177724ddb72d42aa77082e5e875d441d0cf
fadhilabry <fadhil.abr@gmail.com>
2022-04-11 10:46:20 +0700

    initial commit

---
+++ ex5.py
@@ -0,0 +1,25 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lieheight = 74 # inches
+weight = 180 # lbs
+height = 74 #inches
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+ # this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+#studuy drills
+height_in_cm = height * 2.5
+weight_in_kg = weight * 0.5
+
+print(f"He's {height_in_cm} cm tall.")
+print(f"He's {weight_in_kg} kg heavy.")
---
+++ test.py
@@ -0,0 +1,2 @@
+print("hati hati ada apa dibelakang")
+print(3+7*8+2/2)
